b30337aa771d0a6625ec3b4ece30801051f05b24
Ömer Faruk TARTAR <oftartar@hotmail.com>
2023-04-27 21:55:04 +0300

    OFT 27.04.2023 Architecture

---
+++ IKApplication/Presentation/IKApplication.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IKApplication.MVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
