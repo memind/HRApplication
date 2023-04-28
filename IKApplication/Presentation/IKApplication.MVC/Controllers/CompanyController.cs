0ab3d4d1b3f72ec617543291aacfb8696d503452
Sevde Başaran <sevde.basaran@bilgeadamboost.com>
2023-04-28 21:32:42 +0300

    Sevde 28Nisan

---
+++ IKApplication/Presentation/IKApplication.MVC/Controllers/CompanyController.cs
@@ -0,0 +1,37 @@
+﻿//using IKApplication.Domain.Entites;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace IKApplication.MVC.Controllers
+//{
+//    public class CompanyController : Controller
+//    {
+        
+
+        
+//        //private readonly ApplicationDbContext _context;
+//        //public CompanyController(ApplicationDbContext context)
+//        //{
+//        //    _context = context;
+//        //}
+
+//        public IActionResult Index()
+//        {
+//            var companies = _context.Companies.ToList();
+//            return View(companies);
+//        }
+//        public IActionResult Create()
+//        {
+//            return View();
+//        }
+//        [HttpPost]
+//        public IActionResult Create(Company firm)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                _context.Firms.Add(firm);
+//                _context.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+//            return View(firm);
+//        }
+//}
