5e9734f8eeb74fb42f665aba4e867ea1d689de1a
Ömer Faruk TARTAR <oftartar@hotmail.com>
2023-04-29 13:14:27 +0300

    Migration created

--- IKApplication/Presentation/IKApplication.MVC/Program.cs
+++ IKApplication/Presentation/IKApplication.MVC/Program.cs
@@ -1,7 +1,28 @@
+using IKApplication.Domain.Entites;
+using IKApplication.Persistance;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<IKAppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentityCore<AppUser>().AddEntityFrameworkStores<IKAppDbContext>();
+
+builder.Services.AddIdentity<AppUser, IdentityRole<Guid>>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.User.RequireUniqueEmail = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 3;
+}).AddEntityFrameworkStores<IKAppDbContext>().AddDefaultTokenProviders();
 
 var app = builder.Build();
 
