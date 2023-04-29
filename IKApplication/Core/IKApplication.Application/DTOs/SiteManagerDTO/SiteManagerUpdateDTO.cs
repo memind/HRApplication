d42104212b40b52778feac05498b36fc9c95b57c
ALİ SAMET Yurt <alisamet.yurt@bilgeadamboost.com>
2023-04-29 19:00:32 +0300

    UpdateSiteManagerYapıldı, GetByIDyapılacak

---
+++ IKApplication/Core/IKApplication.Application/DTOs/SiteManagerDTO/SiteManagerUpdateDTO.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKApplication.Application.DTOs.SiteManagerDTO
+{
+    public class SiteManagerUpdateDTO
+    {
+        public string Name { get; set; }
+        public string? SecondName { get; set; }
+        public string Surname { get; set; }
+        public string Title { get; set; }   
+        public string? BloodGroup { get; set; }   
+        public string? Profession { get; set; }  
+        public DateTime BirthDate { get; set; }
+        public string IdentityId { get; set; }
+        public string ImagePath { get; set; }  
+        [NotMapped] 
+        public IFormFile? UploadPath { get; set; }
+    }
+}
---
+++ IKApplication/Presentation/IKApplication.MVC/Controllers/SiteManagerController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using IKApplication.Application.AbstractServices;
+using IKApplication.Application.DTOs.SiteManagerDTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKApplication.MVC.Controllers
+{
+    public class SiteManagerController : Controller
+    {
+        private readonly ISiteManagerServices _managerServices;
+        private readonly IMapper _mapper;
+
+        public SiteManagerController(IMapper mapper, ISiteManagerServices managerSerives = null)
+        {
+            _mapper = mapper;
+            _managerServices = managerSerives;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Update (int id)
+        {
+           
+
+        }
+    }
+}
