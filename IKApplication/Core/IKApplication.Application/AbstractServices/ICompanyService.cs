d7f69a7e4b8b0135a5dfa5a60e82d45f57a28cad
Sevde Başaran <sevde.basaran@bilgeadamboost.com>
2023-04-29 14:40:17 +0300

    ICompanyService, CompanyService, Create and Update Company DtOs still working on it

---
+++ IKApplication/Core/IKApplication.Application/AbstractServices/ICompanyService.cs
@@ -0,0 +1,18 @@
+﻿//using IKApplication.Application.DTOs.UserDTOs;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace IKApplication.Application.AbstractServices
+//{
+//    public interface ICompanyService
+//    {
+//        //Company Create
+//        Task Create(CreateCompanyDTO model);
+
+//        //Company Update
+//        Task Update(UpdateCompanyDTO model);
+//    }
+//}
