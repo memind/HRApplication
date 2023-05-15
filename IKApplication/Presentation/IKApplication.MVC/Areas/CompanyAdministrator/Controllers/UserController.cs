788d056d0469a53044436d59c82dcadb15ec9f46
Mehmet Emin Dağ <mehmetemin.dag@bilgeadamboost.com>
2023-05-16 01:36:24 +0300

    - Added CreateCompanyManager() action to CompanyAdministrator/UserController. - Added "New Company Manager" button to UserListViewComponent view. - Added CreateCompanyManager view. - Updated Messages class. - Updated AppUserService.

--- IKApplication/Presentation/IKApplication.MVC/Areas/CompanyAdministrator/Controllers/UserController.cs
+++ IKApplication/Presentation/IKApplication.MVC/Areas/CompanyAdministrator/Controllers/UserController.cs
@@ -1,10 +1,15 @@
-﻿using IKApplication.Application.AbstractServices;
+﻿using AutoMapper;
+using IKApplication.Application.AbstractServices;
+using IKApplication.Application.DTOs.CompanyDTOs;
 using IKApplication.Application.DTOs.UserDTOs;
+using IKApplication.Application.VMs.CompanyVMs;
+using IKApplication.Domain.Enums;
 using IKApplication.Infrastructure.ConcreteServices;
 using IKApplication.MVC.ResultMessages;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NToastNotify;
+using static IKApplication.MVC.ResultMessages.Messages;
 
 namespace IKApplication.MVC.CompanyAdministratorControllers
 {
@@ -14,11 +19,17 @@ namespace IKApplication.MVC.CompanyAdministratorControllers
     {
         private readonly IAppUserService _appUserService;
         private readonly IToastNotification _toast;
+        private readonly ICompanyService _companyService;
+        private readonly ITitleService _titleService;
+        private readonly IMapper _mapper;
 
-        public UserController(IAppUserService appUserSerives, IToastNotification toast)
+        public UserController(IAppUserService appUserSerives, IToastNotification toast, ICompanyService companyService, ITitleService titleService, IMapper mapper)
         {
             _appUserService = appUserSerives;
             _toast = toast;
+            _companyService = companyService;
+            _titleService = titleService;
+            _mapper = mapper;
         }
 
         [HttpGet]
@@ -64,6 +75,34 @@ namespace IKApplication.MVC.CompanyAdministratorControllers
 
             _toast.AddErrorToastMessage(Messages.Errors.Error(), new ToastrOptions { Title = "Updating User" });
             return View(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateCompanyManager()
+        {
+            var user = await _appUserService.GetByUserName(User.Identity.Name);
+            var companies = await _companyService.GetAllCompanies();
+            var titles = await _titleService.GetAllTitles();
+            var companyTitles = titles.Where(x => x.CompanyId == user.CompanyId).ToList();
+
+            var model = new AppUserCreateDTO() { CompanyId = user.CompanyId, Companies = companies, Titles = titles };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCompanyManager(AppUserCreateDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _appUserService.CreateUser(model, "Company Administrator");
+                var user = await _appUserService.GetByUserName(model.Email);
+                await _appUserService.UpdateUser(user);
+                _toast.AddSuccessToastMessage(Messages.CompanyAdmin.Create(model.Email), new ToastrOptions { Title = "Creating Company Manager" });
+                return RedirectToAction("Index");
+            }
+
+            _toast.AddErrorToastMessage(Messages.Errors.Error(), new ToastrOptions { Title = "Creating Company Manager" });
+            return View(model);
         }
     }
 }
