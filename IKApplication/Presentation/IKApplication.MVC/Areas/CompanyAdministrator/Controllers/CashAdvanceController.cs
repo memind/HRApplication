abe86c73c248970cb989ab27db91b91efa733286
tahakayapinar <tahakayapinar@gmail.com>
2023-05-22 22:29:45 +0300

    .

--- IKApplication/Presentation/IKApplication.MVC/Areas/CompanyAdministrator/Controllers/CashAdvanceController.cs
+++ IKApplication/Presentation/IKApplication.MVC/Areas/CompanyAdministrator/Controllers/CashAdvanceController.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+﻿ using AutoMapper;
 using IKApplication.Application.AbstractServices;
 using IKApplication.Application.dtos.CashAdvanceDTOs;
 using IKApplication.Application.dtos.ExpenseDTOs;
